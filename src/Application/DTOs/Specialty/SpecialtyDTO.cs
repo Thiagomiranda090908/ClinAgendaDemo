67952149d5706fe7d13d2fe83b9c2d74f30c54e7
Thiago <thiagomiranda090908@gmail.com>
2025-04-02 22:15:52 -0300

    mudadaa

--- src/Application/DTOs/Specialty/SpecialtyDTO.cs
+++ src/Application/DTOs/Specialty/SpecialtyDTO.cs
@@ -9,6 +9,6 @@ namespace ClinAgenda.src.Application.DTOs.Specialty
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public int ScheduleDuration { get; set; }
+        public int ScheduleDuration { get; set; } 
     }
 }
